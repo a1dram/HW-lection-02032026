d04152aefafea4980d58934a55fed981fc9fd22c
a1dram <aldram54@gmail.com>
2026-03-02 23:53:24 +0300

    Creating a BiLoopList struct and declaring it's methods

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
-
 template< class T >
-struct BiList
+struct BiLoopList
 {
-  T val;
-  BiList< T >* next;
-  BiList< T >* prev;
+  BiList< T >* head;
+  BiList< T >* tail;
+
+  BiLoopList();
+  ~BiLoopList();
+
+  bool empty() const;
+  size_t size() const;
+
+  T* front();
+  const T* front() const;
+  T* back();
+  const T* back() const;
+
+  void push_front(const T& x);
+  void push_back(const T& x);
+  void pop_front();
+  void pop_back();
+
+  void clear();
+  void convert_to_list(const T* a, size_t n);
 };
