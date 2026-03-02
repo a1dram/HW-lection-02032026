ea86d39295016342952f1cc33112909831daa51d
a1dram <aldram54@gmail.com>
2026-03-02 23:56:44 +0300

    Added BiList back and implemented constructor and destructor

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+template< class T >
+struct BiList
+{
+  T val;
+  BiList< T >* next;
+  BiList< T >* prev;
+};
+
 template< class T >
 struct BiLoopList
 {
@@ -23,3 +33,15 @@ struct BiLoopList
   void clear();
   void convert_to_list(const T* a, size_t n);
 };
+
+template< class T >
+BiLoopList< T >::BiLoopList():
+  head(nullptr),
+  tail(nullptr)
+{}
+
+template< class T >
+BiLoopList< T >::~BiLoopList()
+{
+  clear();
+}
