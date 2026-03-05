c951741993bdde609658090c3a69c0864f70dfdc
a1dram <aldram54@gmail.com>
2026-03-05 23:17:59 +0300

    created insert() function

--- main.cpp
+++ main.cpp
@@ -31,3 +31,27 @@ BiList< T >* add(BiList< T >* h, const T& d)
 
   return node;
 }
+
+template< class T >
+BiList< T >* insert(BiList< T >* h, const T& d)
+{
+  // конструктор копирования T::T(const T&)
+  BiList< T >* node = new BiList< T >{ d, nullptr, nullptr };
+
+  if (h == nullptr)
+  {
+    node->next = node;
+    node->prev = node;
+    return node;
+  }
+
+  BiList< T >* after = h->next;
+
+  node->prev = h;
+  node->next = after;
+
+  h->next = node;
+  after->prev = node;
+
+  return node;
+}
