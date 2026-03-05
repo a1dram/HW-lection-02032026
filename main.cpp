542c849e059932c984bca5c2a4bd20f1863eba2b
a1dram <aldram54@gmail.com>
2026-03-05 23:14:53 +0300

    created add() function

--- main.cpp
+++ main.cpp
@@ -7,3 +7,27 @@ struct BiList
   BiList< T >* next;
   BiList< T >* prev;
 };
+
+template< class T >
+BiList< T >* add(BiList< T >* h, const T& d)
+{
+  // конструктор копирования T::T(const T&)
+  BiList< T >* node = new BiList< T >{ d, nullptr, nullptr };
+
+  if (h == nullptr)
+  {
+    node->next = node;
+    node->prev = node;
+    return node;
+  }
+
+  BiList< T >* tail = h->prev;
+
+  node->next = h;
+  node->prev = tail;
+
+  tail->next = node;
+  h->prev = node;
+
+  return node;
+}
